34c55bd3022f48761a238721c36411f28b7cc547
fl1670 <fl1670@gmail.com>
2022-08-24 18:37:26 +0300

    Изменение теста.

--- Tests/main_page/test_main_page_ya.py
+++ Tests/main_page/test_main_page_ya.py
@@ -4,7 +4,7 @@ from Tests.test_base import TestBase
 class TestMainPageYa(TestBase):
 
     def test_temp_1(self):
-        self.APP.main_page.send_keys_in_search_input('Test')
+        self.APP.main_page.send_keys_in_search_input('Test 12312312')
         self.APP.main_page.click_btn_submit()
         self.APP.search.get_text_title()
 
