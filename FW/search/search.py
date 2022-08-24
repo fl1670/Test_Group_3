16f0726309ac24c52d3a59a8b96f98308c575c31
fl1670 <fl1670@gmail.com>
2022-08-24 18:38:33 +0300

    Изменение Search

--- FW/search/search.py
+++ FW/search/search.py
@@ -7,3 +7,5 @@ class Search(AnyPage):
 
     def get_text_title(self):
         return self.get_driver().find_element(By.XPATH, '//li[@data-fast="1"]//h2[contains(@class, "OrganicTitle-LinkText")]').text
+
+    # test
